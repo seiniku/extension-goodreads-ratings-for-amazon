401c877d5f2d6b98c6dbca1d7802d34986113853
Justin <j@keppers.org>
2019-10-16 12:39:58 -0500

    major rework to use api instead of parsing live site

--- app/content.js
+++ app/content.js
@@ -24,93 +24,134 @@ function getTitle()
     return text;
 }
 
-var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
-var tagOrComment = new RegExp('<(?:'
-    // Comment body.
-    + '!--(?:(?:-*[^->])*--+|-?)'
-    // Special "raw text" elements whose content should be elided.
-    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*' + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
-    // Regular name
-    + '|/?[a-z]' + tagBody + ')>', 'gi');
-
-function removeTags(html)
-{
-    var oldHtml;
-    do {
-        oldHtml = html;
-        html = html.replace(tagOrComment, '');
-    } while (html !== oldHtml);
-    return html.replace(/</g, '&lt;');
-}
 /**
- * Get book reviews from ASIN number and show rating
+ * Changes XML to JSON
+ * From: https://gist.github.com/chinchang/8106a82c56ad007e27b1
+ * Modified version from here: http://davidwalsh.name/convert-xml-json
+ * @param {string} xml XML DOM tree
+ */
+function xmlToJson(xml) {
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) {
+      // element
+      // do attributes
+      if (xml.attributes.length > 0) {
+        obj["@attributes"] = {};
+        for (var j = 0; j < xml.attributes.length; j++) {
+          var attribute = xml.attributes.item(j);
+          obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+        }
+      }
+    } else if (xml.nodeType == 3) {
+      // text
+      obj = xml.nodeValue;
+    }
+
+    // do children
+    // If all text nodes inside, get concatenated text from them.
+    var textNodes = [].slice.call(xml.childNodes).filter(function(node) {
+      return node.nodeType === 3;
+    });
+    if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+      obj = [].slice.call(xml.childNodes).reduce(function(text, node) {
+        return text + node.nodeValue;
+      }, "");
+    } else if (xml.hasChildNodes()) {
+      for (var i = 0; i < xml.childNodes.length; i++) {
+        var item = xml.childNodes.item(i);
+        var nodeName = item.nodeName;
+        if (typeof obj[nodeName] == "undefined") {
+          obj[nodeName] = xmlToJson(item);
+        } else {
+          if (typeof obj[nodeName].push == "undefined") {
+            var old = obj[nodeName];
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          obj[nodeName].push(xmlToJson(item));
+        }
+      }
+    }
+    return obj;
+  }
+
+
+/**
+ * Get book reviews from title number and show rating
  * last = boolean. Checks if is the last recursive pass
  */
-function retrieveBookInfo(title, last)
-{
-    var urlGoodreads = "https://www.goodreads.com/book/title?title=" + title;
-    log("Retrieving goodreads info from url: " + urlGoodreads);
+
+function retrieveBookInfo(title, last){
+    var  key = "C8YwtiqnhcXx4f9RKw5i3Q"
+//    var urlGoodreads = "https://www.goodreads.com/book/title.xml?key=C8YwtiqnhcXx4f9RKw5i3Q&title=" + title;
+    var urlGoodreads = "https://www.goodreads.com/book/title.xml?key=C8YwtiqnhcXx4f9RKw5i3Q&title=" + title;
+
+    log("url to search is " + urlGoodreads)
+
     chrome.runtime.sendMessage(
-    {
-        contentScriptQuery: "fetchHtml",
-        url: urlGoodreads
-    }, data =>
-    {
-        let doc = parser.parseFromString(data, "text/html");
-        // GET RATINGS INFO
-        let meta = doc.querySelectorAll("#bookMeta");
-        log("url data retrieved. meta selector: " + meta);
-        log("meta.length: " + meta.length);
-        for (let i = 0, element;
-            (element = meta[i]); i++)
         {
-            log(element);
+            contentScriptQuery: "fetchHtml",
+            url: urlGoodreads
+        }, data =>
+        {
+            let doc = parser.parseFromString(data, "text/xml");
+            var book = doc.getElementsByTagName("book"); //.getElementsByTagName("average_rating");
+            var jsonresult = xmlToJson(doc);
+            if (jsonresult.GoodreadsResponse){
+            log(jsonresult);
+            var stars = jsonresult.GoodreadsResponse.book.average_rating;
+            log(stars);
+            var reviewCount = jsonresult.GoodreadsResponse.book.work.ratings_count;
+            log(reviewCount);
+            } else { log(title + " not found")};
+            var info = buildRatingHTML(title, stars, reviewCount, urlGoodreads);
+            log("info " + info)
+            AppendToChirp(info);
         }
-        if (meta.length === 0)
+)};
+
+/**
+ * Builds the span that contains what we want to insert into the chirpbooks page
+ */
+function buildRatingHTML(title, stars, count, url){
+    var urlGoodreads = "https://www.goodreads.com/book/title?title=" + title;
+
+    var parentSpan = "<br/><span class='goodreadsRating'>";
+    parentSpan += "<span class='stars staticStars'>";
+    var fullStars = stars;
+    i = 0
+    while (fullStars > 0.1)
         {
-            log("Goodreads info not found for this book");
-            return;
-        }
-        meta = meta[0];
-        // CREATE TAGS
-        // Append content
-        var parentSpan = "<br/><span class='goodreadsRating'>";
-//        var parentSpan = "<br/><span id='goodreadsRating' class='goodreadsRating'>";
-        // Stars
-        var stars = meta.querySelectorAll(".stars")[0];
-        if (stars === undefined || stars === null)
-        {
-            log("Cannot find '.stars' info on page");
-            return;
-        }
-        // Create manually to avoid injection
-        parentSpan += "<span class='stars staticStars'>";
-        for (var i = 0; i < stars.children.length; i++)
-        {
-            parentSpan += "<span class='" + stars.children[i].className + "' size=12x12></span>";
+            log("fullStars = " + fullStars)
+            if (fullStars >= 1){
+                parentSpan += "<span class='staticStar p10' size=12x12></span>";
+                fullStars--;
+                i++;
+            }
+            if (fullStars < 1){
+                if (fullStars >= 0.5){
+                    parentSpan += "<span class='staticStar p6' size=12x12></span>";
+               } else {
+                parentSpan += "<span class='staticStar p3' size=12x12></span>";
+               }
+                if (i < 0.5){
+                    parentSpan += "<span class='staticStar p0' size=12x12></span>";
+                }
+                fullStars = 0
+            }
         }
         parentSpan += "</span>";
-        // Spacing
         parentSpan += "<span class='a-letter-space'></span><span class='a-letter-space'></span>";
-        // Review count and link to Goodreads
-        var averageHtml = meta.querySelectorAll("[itemprop=ratingValue]")[0].textContent;
-        var votesHtml = meta.querySelectorAll("[itemprop=ratingCount]")[0].parentNode.textContent;
-        log(votesHtml);
-        log(removeTags(votesHtml)
-            .trim());
-        // Clean html
-        var reviewCount = removeTags(averageHtml)
-            .trim() + " from " + removeTags(votesHtml)
-            .trim();
-        parentSpan += "<a href='" + urlGoodreads + "'>" + reviewCount + "</a>";
+        parentSpan += "<a href='" + urlGoodreads + "'>" + stars + " from " +  Number(count).toLocaleString() + " ratings" + "</a>";
         parentSpan += "</span>";
-        // FINALLY APPEND TO PAGE
         log("Span object : " + parentSpan);
-        // Chirp
-        cleanSpan = Sanitizer.createSafeHTML(parentSpan)
-        AppendToChirp(cleanSpan);
-    });
-}
+        cleanSpan = Sanitizer.createSafeHTML(parentSpan);
+        log("cleanspan" + cleanSpan)
+        return cleanSpan;
+};
+
 /**
  * Appends ratings to Chirp page
  */
@@ -136,7 +177,6 @@ function AppendToChirp(contentSpan)
 function checkIfBook()
 {
     log("checkIfBook");
-    // Chirp
     return document.querySelectorAll(".credits") !== null || document.querySelectorAll(".book-title") !== null;
     //document.querySelectorAll("a[class^='promotion_card-module__title___']")
 }
@@ -145,52 +185,52 @@ function checkIfBook()
  */
 // Try to get the book info as soon as possible
 
-
-
-    var asinFound = false;
-    var startTime = Date.now();
-    if (checkIfBook())
+var titleFound = false;
+var startTime = Date.now();
+if (checkIfBook())
+{
+    var titleChecker = window.setInterval(function()
     {
-        var asinChecker = window.setInterval(function()
+        intervalsPassed++;
+        log("Inverval number " + intervalsPassed);
+        var title = getTitle();
+        // Is title found, stop and retrieve book info
+        if (title !== false)
+        { // title found
+            window.clearInterval(titleChecker);
+            titleFound = true;
+            retrieveBookInfo(title,false);
+        }
+        // After 10 seconds stop checking for title
+        var timeInSeconds = Math.floor((Date.now() - startTime)) / 1000;
+        var stopChecks = timeInSeconds > 10;
+        if (stopChecks === true)
         {
-            intervalsPassed++;
-            log("Inverval number " + intervalsPassed);
-            var asin = getTitle();
-            // Is ASIN found, stop and retrieve book info
-            if (asin !== false)
-            { // ASIN found
-                window.clearInterval(asinChecker);
-                asinFound = true; // No need to check anymore
-                retrieveBookInfo(asin, false);
-            }
-            // After 10 seconds stop checking for ASIN
-            var timeInSeconds = Math.floor((Date.now() - startTime)) / 1000;
-            var stopChecks = timeInSeconds > 10;
-            if (stopChecks === true)
-            {
-                window.clearInterval(asinChecker);
-            }
-        }, 500);
-        /**
-         * After loading page check if ASIN was found or try once more
-         */
-        document.addEventListener("DOMContentLoaded", function()
+            window.clearInterval(titleChecker);
+        }
+    }, 500);
+
+    /**
+     * After loading page check if title was found or try once more
+     */
+    document.addEventListener("DOMContentLoaded", function()
+    {
+        log("Page loaded in " + (Date.now() - startTime) + " ms");
+        if (!titleFound)
         {
-            log("Page loaded in " + (Date.now() - startTime) + " ms");
-            if (!asinFound)
-            {
-                // Always remove interval (if ASIN not found, should exists)
-                window.clearInterval(asinChecker);
-                var asin = getTitle();
-                log("Document load asin found? : " + asin);
-                if (asin !== false)
-                { // ASIN found
-                    retrieveBookInfo(asin, false);
-                }
-                else
-                {
-                    log("Book not found. THE END.");
-                }
+            // Always remove interval (if title not found, should exists)
+            window.clearInterval(titleChecker);
+            var title = getTitle();
+            log("Document load title found? : " + title);
+            if (title !== false)
+            { // title found
+                retrieveBookInfo(title,false);
+                titleFound = true;
             }
-        });
-    }
+            else
+            {
+                log("Book not found. THE END.");
+            }
+        }
+    });
+}
